a915fbbcc663a8d4de51f020145da2429ca8a177
teddy <teddy.herzfeld@icloud.com>
2017-03-16 11:27:49 -0400

    Initial Commit

---
+++ motdedit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  motdedit
+//
+//  Created by Teddy Herzfeld on 3/16/17.
+//  Copyright © 2017 Teddy Herzfeld. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
